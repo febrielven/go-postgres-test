a9358feef7be26435e2976b35f21ecedf658ac87
unknown <febrianto.febri@bfi.co.id>
2020-09-12 08:59:47 +0700

    create connection

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/febrielven/go-postgres-test/api/middleware"
 )
 
 func main() {
-	fmt.Println(" Hello Word")
+	db := middleware.CreateConnection()
+
+	defer db.Close()
+
+	fmt.Println("Starting server on the port 3999...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
